d4b3e8476fd61d68292b3c5f416397d003d39f40
vatsal <vatsalpatel1073@gmail.com>
2021-09-20 15:05:56 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(cors(origin));
+
+app.listen(process.env.PORT, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
